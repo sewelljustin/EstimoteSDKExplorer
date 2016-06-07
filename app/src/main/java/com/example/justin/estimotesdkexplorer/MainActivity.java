7d42e0d53ef3749d4b3c64729ea7150479383ba0
Justin Sewell <sewell_justin@columbusstate.edu>
2016-06-07 12:08:40 -0400

    Eddystone scanning commit

--- app/src/main/java/com/example/justin/estimotesdkexplorer/MainActivity.java
+++ app/src/main/java/com/example/justin/estimotesdkexplorer/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.justin.estimotesdkexplorer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.SystemRequirementsChecker;
@@ -18,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
     private Map<String, Eddystone> beaconMap;
     private List<Eddystone> beaconList;
 
+    private ListView listView;
+    private ArrayAdapter<String> listAdapter;
+
+    /** Scan ID of Eddystone scanner */
+    String scanId;
+
+    /** Flag to indicate scanning activity */
+    boolean scanning;
+
+    /** Flag to indicate service ready */
+    boolean serviceReady;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,24 +41,34 @@ public class MainActivity extends AppCompatActivity {
         beaconMap = new HashMap<>();
         beaconList = new ArrayList<>();
 
+        listView = (ListView) findViewById(R.id.listView);
+        listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(listAdapter);
+
         beaconManager = new BeaconManager(getApplicationContext());
+        beaconManager.setEddystoneListener(new BeaconManager.EddystoneListener() {
+            @Override
+            public void onEddystonesFound(List<Eddystone> list) {
+                for (Eddystone eddystone : list) {
+                    String macAddress = eddystone.macAddress.toStandardString();
+                    if (!beaconMap.containsKey(macAddress)) {
+                        beaconMap.put(macAddress, eddystone);
+                        beaconList.add(eddystone);
+
+                        listAdapter.add(macAddress);
+                    }
+                }
+            }
+        });
+
         beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
             @Override
             public void onServiceReady() {
-                beaconManager.setEddystoneListener(new BeaconManager.EddystoneListener() {
-                    @Override
-                    public void onEddystonesFound(List<Eddystone> list) {
-                        for (Eddystone eddystone : list) {
-                            String macAddress = eddystone.macAddress.toStandardString();
-                            if (!beaconMap.containsKey(macAddress)) {
-                                beaconMap.put(macAddress, eddystone);
-                                beaconList.add(eddystone);
-                            }
-                        }
-                    }
-                });
+                serviceReady = true;
 
-                beaconManager.startEddystoneScanning();
+                if (!scanning) {
+                    startScanning();
+                }
             }
         });
     }
@@ -52,6 +77,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
+        if (serviceReady && !scanning) {
+            startScanning();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (scanning) {
+            stopScanning();
+        }
+    }
+
+    private void startScanning() {
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
+        scanId = beaconManager.startEddystoneScanning();
+        scanning = true;
+    }
+
+    private void stopScanning() {
+        beaconManager.stopEddystoneScanning(scanId);
+        scanning = false;
     }
 }
